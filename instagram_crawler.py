6140859365f4cf72bbcb0a92f55ceb012a9cc1b0
pcchencode <zxp791228@gmail.com>
2022-09-21 14:39:52 +0800

    fix redundant code

--- instagram_crawler.py
+++ instagram_crawler.py
@@ -22,9 +22,12 @@ class ig_photo_crawler():
         print(headers['cookie'])
         s = requests.session()
         r = s.get(url, headers=headers)
-        print(r.text)
         soup = bs4.BeautifulSoup(r.text, "html.parser")
         js = json.loads(soup.text)
+        print(js)
+        if 'message' in js and js['message']=='Please wait a few minutes before you try again.':
+            print("Blocked by Instagram API, try again few minutes later")
+            os._exit(0)
         ig_id = re.search('(?<=profilePage_).*', js['logging_page_id'], re.IGNORECASE).group(0)
         return ig_id
 
@@ -33,42 +36,43 @@ class ig_photo_crawler():
         has_next_page = False
         post_remained = self.num_post
         while True:
-            variables = {"id":ig_id, "first":self.first}
+            if post_remained <= 10: #分10頁10頁抓
+                f = post_remained
+            else:
+                f = 10
+            variables = {"id":ig_id, "first":f}
             if has_next_page:
                 variables['after'] = end_cursor
             # varibles['after'] = js['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
             variables = json.dumps(variables, separators=(',', ':'))
-            profile_url = "https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables=%s"
-            print(urllib.parse.quote(variables))
-
-            url = profile_url % urllib.parse.quote(variables)
-            print(url)
+            profile_url = f"https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={variables}"
+            print(profile_url)
 
             # 讀取可能會被限速...要小心！
-            headers = {"user-agent":self.agent,"cookie":"sessionid=%s"}
-            headers['cookie'] = headers['cookie'] % self.sid[random.randrange(0,len(self.sid))]
-            print(headers['cookie'])
-            Url = req.Request(url, headers=headers)
-            with req.urlopen(Url) as response:
-                data = response.read().decode("utf-8")
-            soup = bs4.BeautifulSoup(data, "html.parser")
-            js = json.loads(soup.string)
+            headers = {"user-agent":self.agent, "cookie":f"sessionid={self.sid}"}
+            s = requests.session()
+            r = s.get(profile_url, headers=headers)
+
+            soup = bs4.BeautifulSoup(r.text, "html.parser")
+            js = json.loads(soup.text)
+            if 'message' in js and js['message']=='Please wait a few minutes before you try again.':
+                print("Blocked by Instagram API, try again few minutes later")
+                os._exit(0)
             # 用來判斷要不要爬取下一夜、以及下一頁的開頭
             has_next_page = js['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
             end_cursor = js['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
 
             len_post = len(js['data']['user']['edge_owner_to_timeline_media']['edges'])
-            post_remained -= 100
-            print(f"目前profile頁面貼文數：{len_post}")
+            post_remained -= 10
+            print(has_next_page, end_cursor, len_post)
             for i in range(0, len_post):
                 js['data']['user']['edge_owner_to_timeline_media']['edges'][i]['node']
                 short_code = js['data']['user']['edge_owner_to_timeline_media']['edges'][i]['node']['shortcode']
                 print(short_code)
                 shortcode_list.append(short_code)
-
             if has_next_page==False:
                 break
-            elif post_remained<0:
+            if post_remained<0:
                 break
             print("time sleeping...")
             time.sleep(random.randrange(10,20))
@@ -77,16 +81,17 @@ class ig_photo_crawler():
     def get_photo_urls_from_shortcodes(self, shortcodes):
         shortcode_photo_url_pairs = []
         for new_sc in shortcodes:
-            url = f"https://www.instagram.com/p/{new_sc}?__a=1"
+            url = f"https://www.instagram.com/p/{new_sc}/?__a=1&__d=dis"
             print(url)
-            headers = {"user-agent":self.agent,"cookie":"sessionid=%s"}
-            headers['cookie'] = headers['cookie'] % self.sid[random.randrange(0,len(self.sid))] #隨機挑sessionid
-            print(headers['cookie'])
-            Url = req.Request(url, headers=headers)
-            with req.urlopen(Url) as response:
-                data = response.read().decode("utf-8")
-            soup = bs4.BeautifulSoup(data, "html.parser")
+            headers = {"user-agent":self.agent, "cookie":f"sessionid={self.sid}"}
+            s = requests.session()
+            r = s.get(url, headers=headers)
+
+            soup = bs4.BeautifulSoup(r.text, "html.parser")
             js = json.loads(soup.text)
+            if 'message' in js and js['message']=='Please wait a few minutes before you try again.':
+                print("Blocked by Instagram API, try again few minutes later")
+                os._exit(0)
 
             if 'edge_sidecar_to_children' in js['graphql']['shortcode_media'].keys():
                 #此篇是多分頁貼文
